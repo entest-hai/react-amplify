0601313cbc7b7a88d76edbe52d1e6ebbd82cdd0e
Tran Minh Hai <hai@bio-rithm.com>
2021-01-05 17:55:40 +0700

    both datastore and graphql queries with filter

--- src/test/TestDBQuery.js
+++ src/test/TestDBQuery.js
@@ -1,6 +1,7 @@
 // 05 JAN 2020 Query DynamoDB
-// 1. Use Datastore
-// 2. Use Graphql
+// 1. Use Datastore with filter
+// 2. Use Graphql with filter
+
 
 import React, {useEffect, useState} from "react";
 import Amplify, {API, graphqlOperation} from "aws-amplify";
@@ -16,12 +17,24 @@ async function testDatstoreQuery() {
 }
 
 const TestDBQueryView = () => {
+    let filter = {
+        name: {
+            eq: "A202.csv" // filter priority = 1
+            }
+        };
     const [records, setRecords] = React.useState([])
     const fetchRecords = async () => {
         try {
+            // Graphql query
             // const recordData = await API.graphql(graphqlOperation(listRecords));
-            // const dbRecords = recordData.data.listRecords.items;
-            const dbRecords = await DataStore.query(Record, c => c.name("eq", "A202.csv"));
+            // const recordData = await API.graphql({ query: listRecords });
+            const recordData = await API.graphql({ query: listRecords, variables: { filter: filter}});
+            const dbRecords = recordData.data.listRecords.items;
+
+            // DataStore query
+            // const dbRecords = await DataStore.query(Record, c => c.name("eq", "A202.csv"));
+
+            //
             setRecords(dbRecords);
             console.log(dbRecords);
         } catch (error) {
